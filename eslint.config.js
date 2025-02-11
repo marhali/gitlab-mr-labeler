1762d6468add0569a6ba1f14b0814d91f5a11609
marhali <me@marhali.de>
2025-02-11 17:59:59 +0100

    feat: configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import eslintJs from '@eslint/js';
+import typescriptEslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginVitest from '@vitest/eslint-plugin';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default typescriptEslint.config(
+  eslintJs.configs.recommended,
+  typescriptEslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.js'],
+        },
+
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslintPluginUnicorn.configs['flat/recommended'],
+  eslintPluginVitest.configs.recommended,
+  eslintPluginPrettierRecommended,
+);
