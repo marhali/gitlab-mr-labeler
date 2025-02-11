5b50704a1f10a38c0971d23cd96b1e157f3a0b46
marhali <me@marhali.de>
2025-02-11 17:58:58 +0100

    feat: setup vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  build: {
+    target: 'esnext',
+    minify: false,
+    lib: {
+      entry: 'src/bootstrap.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['node:child_process', 'node:util'],
+    },
+  },
+  test: {
+    // ...
+  },
+});
