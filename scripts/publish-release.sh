89a28c237cc81b401d0a339f85503865ba2f78d6
marhali <me@marhali.de>
2025-02-18 19:25:16 +0100

    build: sign docker image using cosign

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -8,19 +8,38 @@ REPOSITORY="$DOCKER_USERNAME/gitlab-mr-labeler"
 
 echo ""
 echo "| P U B L I S H    R E L E A S E"
-echo "| Version:    $VERSION"
-echo "| Branch:     $BRANCH"
-echo "| Repository: $REPOSITORY"
+echo "| Version:       $VERSION"
+echo "| Branch:        $BRANCH"
+echo "| Repository:    $REPOSITORY"
+echo "| URI:           $URI"
+echo "|"
+
+if test $BRANCH = "main"
+then
+  PRIMARY_TAG="$REPOSITORY:$VERSION"
+  SECONDARY_TAG="$REPOSITORY:latest"
+else
+  PRIMARY_TAG="$REPOSITORY:$BRANCH-$VERSION"
+  SECONDARY_TAG="$REPOSITORY:$BRANCH"
+fi
+
+URI = "docker.io/$REPOSITORY:$PRIMARY_TAG"
+
+echo "| URI:           $URI"
+echo "| Primary Tag:   $PRIMARY_TAG"
+echo "| Secondary Tag: $SECONDARY_TAG"
 echo ""
 
 echo "Login to docker registry..."
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 echo "Build and push docker image..."
+docker buildx build --sbom=true --provenance=true -t "$PRIMARY_TAG" -t "$SECONDARY_TAG" --push .
 
-if test $BRANCH = "main"
-then
-  docker buildx build --sbom=true --provenance=true -t "$REPOSITORY:$VERSION" -t "$REPOSITORY:latest" --push .
-else
-  docker buildx build --sbom=true --provenance=true -t "$REPOSITORY:$BRANCH-$VERSION" -t "$REPOSITORY:$BRANCH" --push .
-fi
+echo "Sign docker image with cosign..."
+cosign sign --key env://COSIGN_KEY $URI
+
+echo "Verify docker image..."
+docker buildx imagetools inspect $URI
+docker pull $URI
+cosign verify --key .certs/cosign.pub $URI
